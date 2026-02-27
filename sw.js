58a97d2d3aa0b51495fa4eadf4737056f977b5c7
akasiamoon <akasiamoonshadow@gmail.com>
2026-02-27 01:38:49 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'alinor-cache-v1';
+const CACHE_NAME = 'moon-hearth-v2'; // Changed v1 to v2
 const urlsToCache = [
   './',
   './index.html',
@@ -9,16 +9,34 @@ const urlsToCache = [
   './icon_512.jpg'
 ];
 
+// Install the Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate and delete OLD caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
